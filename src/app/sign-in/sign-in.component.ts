f54c7277153dc9a6e28381b6e2dbad1c19ab6b77
SANJAY KUMAR <sanjaykumar2ygl@gmail.com>
2022-12-04 00:48:58 +0530

    Google SignIn Done :D

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signInWithGoogle() {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        console.log({ token, user });
+        this.route.navigate(['Home']);
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
   }
 
 }
