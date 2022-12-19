8def7a9c47c0ef20fd415c2f567c4fbff71984a1
SANJAY KUMAR <sanjaykumar2ygl@gmail.com>
2022-12-19 12:39:29 +0530

    Fixed Bugs in counting Views

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -12,6 +12,7 @@ export class VideoComponent implements OnInit, OnDestroy {
   id!: string;
   videoData: any;
   timer: any;
+  loader: boolean = true;
   constructor(private route: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit(): void {
@@ -20,9 +21,12 @@ export class VideoComponent implements OnInit, OnDestroy {
     console.log(this.videoData);
     setTimeout(() => {
       if (this.api.isLoggedIn()) {
-        this.checkView();
+        setTimeout(() => this.checkView(), 1000);
       }
-    }, 1000);
+      this.loader = false;
+      this.videoData.time = this.findTime(this.videoData.time);
+      console.log(this.videoData);
+    }, 2000);
   }
 
   ngOnDestroy(): void {
@@ -43,16 +47,63 @@ export class VideoComponent implements OnInit, OnDestroy {
     }, 2000);
     this.timer = setInterval(() => {
       let current = Math.floor(player.currentTime);
-      let total = Math.floor(player.duration / 2);
+      let total = Math.ceil(player.duration / 2);
       console.log({ total, current });
       if (current == total) {
         console.log('COUNTED AS A VIEW!');
         clearInterval(this.timer);
         this.videoData.views += 1;
-        viewedArr.push(this.id);
-        this.api.writeUserData(userData[uid].name, uid, userData[uid].liked, viewedArr, userData[uid].uploaded, userData[uid].joined,);
+        if (!viewedArr.includes(this.id)) {
+          viewedArr.push(this.id);
+          this.api.writeUserData(userData[uid].name, uid, userData[uid].profileImage, userData[uid].liked, viewedArr, userData[uid].uploaded, userData[uid].notifications, userData[uid].joined,);
+        }
         this.api.writeVideoData(this.videoData.url, this.videoData.title, this.videoData.videoId, this.videoData.description, this.videoData.likes, this.videoData.views, this.videoData.userId, this.videoData.time);
       }
     }, 1000);
+  }
+
+  findTime(prevDate: any) {
+    var currDate = new Date().getTime();
+    var result;
+    console.log({ prevDate, currDate });
+    var diffInMs = getDifferenceInMs(prevDate, currDate);
+    var seconds = Math.floor(diffInMs / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+    var weeks = Math.floor(days / 7);
+    var months = Math.floor(days / 30);
+    var years = Math.floor(months / 12);
+    console.log({ years, months, weeks, days, hours, minutes, seconds });
+    if (years != 0) {
+      result = years + " ";
+      result += (years == 1) ? "year" : "years";
+    } else if (months != 0) {
+      result = months + " ";
+      result += (months == 1) ? "month" : "months";
+    } else if (weeks != 0) {
+      result = weeks + " ";
+      result += (weeks == 1) ? "week" : "weeks";
+    } else if (days != 0) {
+      result = days + " ";
+      result += (days == 1) ? "day" : "days";
+    } else if (hours != 0) {
+      result = hours + " ";
+      result += (hours == 1) ? "hour" : "hours";
+    } else if (minutes != 0) {
+      result = minutes + " ";
+      result += (minutes == 1) ? "minute" : "minutes";
+    } else {
+      result = seconds + " ";
+      result += (seconds == 1) ? "second" : "seconds";
+    }
+    console.log('Final', result);
+
+    function getDifferenceInMs(date1: number, date2: number) {
+      const diffInMs = Math.abs(date2 - date1);
+      return diffInMs;
+    }
+
+    return result;
   }
 }
