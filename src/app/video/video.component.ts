8e29064f4be3ebb64b6ca646f3c5c06eb6c4379d
SANJAY KUMAR <sanjaykumar2ygl@gmail.com>
2022-12-25 03:29:08 +0530

    Updated Layout and other features: Like and Share

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -14,20 +14,38 @@ export class VideoComponent implements OnInit, OnDestroy {
   timer: any;
   loader: boolean = true;
   originalTime: any;
-  constructor(private route: ActivatedRoute, private api: ApiService) { }
+  isLoggedIn: boolean = false;
+  currUserData: any;
+  currUserId: any;
+  userData: any;
+  likedArr!: Array<String>;
+  shareBtn = 'Share';
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
     this.id = "" + this.route.snapshot.paramMap.get('id');
     this.videoData = this.api.readSpecificVideoData(this.id);
-    console.log(this.videoData);
+    setTimeout(() => {
+      this.userData = this.api.readSpecificUserData(this.videoData.userId);
+      console.log(this.videoData);
+      console.log('Ownerruuu', this.userData);
+    }, 2000);
     setTimeout(() => {
       if (this.api.isLoggedIn()) {
-        setTimeout(() => this.checkView(), 1000);
+        this.isLoggedIn = true;
+        this.currUserId = this.api.getUserDetails().UID;
+        this.currUserData = this.api.readSpecificUserData(this.currUserId);
+        setTimeout(() => {
+          this.checkView();
+          console.log('LikedddArrrr', this.currUserData.liked);
+          this.likedArr = this.currUserData.liked;
+        }, 2000);
       }
-      this.loader = false;
       this.originalTime = this.videoData.time;
       this.videoData.time = this.findTime(this.videoData.time);
       console.log(this.videoData);
+      setTimeout(() => this.loader = false, 1000);
     }, 2000);
   }
 
@@ -35,18 +53,45 @@ export class VideoComponent implements OnInit, OnDestroy {
     clearInterval(this.timer);
   }
 
+  getLike(e: any) {
+    if (e.target.checked) {
+      if (!this.isLoggedIn) {
+        let prev = this.router.url;
+        sessionStorage.setItem('Prev', prev);
+        this.router.navigate(['SignIn']);
+        this.videoData.likes += 1;
+        this.likedArr.push(this.id);
+      } else {
+        this.videoData.likes += 1;
+        this.likedArr.push(this.id);
+        console.log('Liked', this.currUserData, this.likedArr);
+      }
+    } else {
+      this.videoData.likes -= 1;
+      // likedArr.pop();
+      this.likedArr.splice(this.likedArr.indexOf(this.id), 1);
+      console.log('Disliked', this.currUserData, this.likedArr);
+    }
+
+    this.api.writeVideoData(this.videoData.url, this.videoData.title, this.videoData.videoId, this.videoData.description, this.videoData.likes, this.videoData.views, this.videoData.userId, this.originalTime);
+
+    if (this.likedArr.length == 0) { this.likedArr.push(''); }
+    this.api.writeUserData(this.currUserData.name, this.currUserData.userId, this.currUserData.profileImage, this.likedArr, this.currUserData.viewed, this.currUserData.uploaded, this.currUserData.notifications, this.currUserData.joined);
+  }
+
   checkView() {
     var player = document.getElementsByTagName('video')[0];
-    var uid = '' + this.api.getUserDetails().UID;
-    var userData: any = this.api.readUserData();
-    var viewedArr: String[];
+    var viewedArr: Array<String>;
     setTimeout(() => {
-      viewedArr = userData[uid].viewed;
+      viewedArr = this.currUserData.viewed;
+      console.log(this.currUserId, this.currUserData.viewed);
       if (viewedArr[0] == "") {
         console.log('NULLLLLLL');
         viewedArr.shift();
+      } else {
+        console.log(viewedArr);
       }
-    }, 2000);
+    }, 100);
     this.timer = setInterval(() => {
       let current = Math.floor(player.currentTime);
       let total = Math.ceil(player.duration / 2);
@@ -57,7 +102,7 @@ export class VideoComponent implements OnInit, OnDestroy {
         this.videoData.views += 1;
         if (!viewedArr.includes(this.id)) {
           viewedArr.push(this.id);
-          this.api.writeUserData(userData[uid].name, uid, userData[uid].profileImage, userData[uid].liked, viewedArr, userData[uid].uploaded, userData[uid].notifications, userData[uid].joined,);
+          this.api.writeUserData(this.currUserData.name, this.currUserId, this.currUserData.profileImage, this.currUserData.liked, viewedArr, this.currUserData.uploaded, this.currUserData.notifications, this.currUserData.joined,);
         }
         this.api.writeVideoData(this.videoData.url, this.videoData.title, this.videoData.videoId, this.videoData.description, this.videoData.likes, this.videoData.views, this.videoData.userId, this.originalTime);
       }
@@ -107,5 +152,10 @@ export class VideoComponent implements OnInit, OnDestroy {
     }
 
     return result;
+  }
+
+  copyLinkToClipboard() {
+    navigator.clipboard.writeText(location.href);
+    this.shareBtn = 'Copied';
   }
 }
