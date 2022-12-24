ca6335f792b3d6d967f9147fc387e2b4854806b6
SANJAY KUMAR <sanjaykumar2ygl@gmail.com>
2022-12-24 14:12:32 +0530

    Added Paths for Notification and Search Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HistoryComponent } from './history/history.component';
 import { HomeComponent } from './home/home.component';
 import { LibraryComponent } from './library/library.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { TestLayoutComponent } from './test-layout/test-layout.component';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
@@ -13,9 +15,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
   { path: 'watch/:id', component: VideoComponent },
+  { path: 'search/:id', component: SearchResultsComponent },
   { path: 'Upload', component: UploadVideoComponent },
   { path: 'SignIn', component: SignInComponent },
   { path: 'profile/:id', component: ProfileComponent },
+  { path: 'notifications', component: NotificationsComponent },
   { path: 'library', component: LibraryComponent },
   { path: 'history', component: HistoryComponent },
   { path: 'Test', component: TestLayoutComponent }
