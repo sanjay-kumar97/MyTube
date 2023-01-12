faae3d2e72fa969cd9c2714ed1d8546c6e2bcce6
SANJAY KUMAR <sanjaykumar2ygl@gmail.com>
2023-01-12 13:11:43 +0530

    Small Tweaks

--- src/app/test-layout/test-layout.component.ts
+++ src/app/test-layout/test-layout.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { AppComponent } from '../app.component';
 
 @Component({
+  providers: [AppComponent],
   selector: 'app-test-layout',
   templateUrl: './test-layout.component.html',
   styleUrls: ['./test-layout.component.scss']
 })
 export class TestLayoutComponent implements OnInit {
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private app: AppComponent) { }
   currUserData: any;
   userData: any;
   notifyArr: any;
   videoData: any;
+  count!: number;
   loader: boolean = true;
   ngOnInit(): void {
+    this.count = 5;
     this.userData = this.api.readUserData();
     // setTimeout(() => this.currUserData = this.api.readSpecificUserData('' + this.api.getUserDetails().UID), 2000);
     this.videoData = this.api.readVideoData();
